94bf87e55af75ed043e8fa13fd8f146f61785e28
thayamizu <t.hayamizu@outlook.com>
2016-09-12 01:20:51 +0900

    implements prototype.

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as cp  from 'child_process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,15 +15,89 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
+    let google = vscode.commands.registerCommand('web-search.google', () => {
+        vscode.window.showInputBox().then((text)=>{
+            searchGoogle(text);
+        })
+    });
+    let msdn = vscode.commands.registerCommand('web-search.msdn', () => {
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        vscode.window.showInputBox().then((text)=>{
+            searchMSDN(text);
+        })
+    });
+    let reddit = vscode.commands.registerCommand('web-search.reddit', () => {
+        // Display a message box to the user
+        vscode.window.showInputBox().then((text)=>{
+            searchReddit(text);
+        })
+    });
+    let vscRef = vscode.commands.registerCommand('web-search.vscode', () => {
+        // Display a message box to the user
+        vscode.window.showInputBox().then((text)=>{
+            searchVSCode(text);
+        })
+    });
+    let stackOVerFlow = vscode.commands.registerCommand('web-search.stackoverflow', () => {
+        // Display a message box to the user
+        vscode.window.showInputBox().then((text)=>{
+            searchStackOverflow(text);
+        })
     });
 
-    context.subscriptions.push(disposable);
+
+    context.subscriptions.push(google);
+    context.subscriptions.push(reddit);
+    context.subscriptions.push(vscRef);
+    context.subscriptions.push(stackOVerFlow);
 }
+
+function searchGoogle(keyword:string) {
+    const baseUrl = "http://www.google.com/search?q=";
+    execSearch(baseUrl, keyword);
+}
+
+function searchMSDN(keyword:string) {
+    const baseUrl = "https://social.msdn.microsoft.com/Search/en-US?query=";
+    execSearch(baseUrl, keyword);
+    
+}
+
+function searchReddit(keyword:string) {
+    const baseUrl = "https://www.reddit.com/r/node/search?q=";
+    const url = baseUrl + encode(keyword);
+    execSearch(baseUrl, keyword);
+
+}
+
+function searchVSCode(keyword:string) {
+    const baseUrl="https://code.visualstudio.com/Search?q=";
+    execSearch(baseUrl, keyword);
+}
+
+function searchStackOverflow(keyword:string) {
+    const baseUrl="http://stackoverflow.com/search?q=";
+    execSearch(baseUrl, keyword);
+}
+
+function execSearch(baseUrl:string, keyword:string) {
+    const url = baseUrl + encode(keyword);
+    const exePath = "C:\\Program Files\\Waterfox\\waterfox.exe";
+    cp.spawn(exePath, [url]);  
+}
+
+function encode(text : string ) {
+    let keywords = text.split(' ');
+    let query: string  ="";
+    keywords.forEach(element => {
+            if (query != "") {
+                query +="+";
+            }
+            query += encodeURIComponent(element);
+    });
+
+    return query;
+} 
 
 // this method is called when your extension is deactivated
 export function deactivate() {
